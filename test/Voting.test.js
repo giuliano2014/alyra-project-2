4b74db90601846c167dd4facc8f4f1f5c2ab91e1
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-02-24 21:20:19 +0100

    Test modifier onlyVoters

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -8,13 +8,39 @@ contract('Voting', accounts => {
     let votingInstance;
     const owner = accounts[0];
     const voter1 = accounts[1];
+    const voter2 = accounts[2];
 
-    describe ('Add voter test', () => {
+    describe('test onlyVoter modifier', () => {
+        beforeEach(async () => {
+            votingInstance = await Voting.new({ from: owner });
+            await votingInstance.addVoter(voter1, { from: owner });
+            await votingInstance.addVoter(voter2, { from: owner });
+        });
+
+        it("should allow access for registered voter", async() => {
+            await votingInstance.getVoter(voter1, { from: voter1 });
+            await votingInstance.getVoter(voter1, { from: voter2 });
+            await votingInstance.getVoter(voter2, { from: voter1 });
+        });
+
+        it('should revert when non-registered voter tries to access', async () => {
+            await expectRevert(
+                votingInstance.getVoter(voter1, { from: owner }),
+                "You're not a voter"
+            );
+            await expectRevert(
+                votingInstance.getVoter(voter2, { from: owner }),
+                "You're not a voter"
+            );
+        });
+    });
+
+    describe('test addVoter function', () => {
         beforeEach(async () => {
             votingInstance = await Voting.new({ from: owner });
         });
     
-        it('should add a voter', async() => {
+        it('should add a voter', async () => {
             await votingInstance.addVoter(voter1, { from: owner });
             const voter = await votingInstance.getVoter(voter1, { from: voter1 });
     
@@ -23,7 +49,7 @@ contract('Voting', accounts => {
             expect(voter.votedProposalId).to.be.bignumber.equal(new BN(0));
         });
     
-        it('should revert when adding a registered voter', async() => {
+        it('should revert when adding a registered voter', async () => {
             await votingInstance.addVoter(voter1, { from: owner });
     
             await expectRevert(
@@ -32,13 +58,13 @@ contract('Voting', accounts => {
             );
         });
     
-        it('should revert when adding a voter by non-owner account', async() => {
+        it('should revert when adding a voter by non-owner account', async () => {
             await expectRevert(
-                votingInstance.addVoter(voter1, { from: voter1 }),
+                votingInstance.addVoter(voter2, { from: voter1 }),
                 'Ownable: caller is not the owner'
             );
         });
-    })
+    });
 
 });
 
